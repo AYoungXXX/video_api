7f33e1d9c7ab56f65ef50c9ed078807d46f2b094
mac <hcysir@gmail.com>
2025-11-23 16:55:40 +0800

    hono+cheerio重构

--- vercel_entry.js
+++ vercel_entry.js
@@ -1,8 +1,13 @@
 // 加载 Polyfills
 require('./polyfills');
 
-const { handle } = require('hono/vercel');
+const { getRequestListener } = require('@hono/node-server');
 const app = require('./app');
 
-// 使用 Hono 的 Vercel 适配器
-module.exports = handle(app);
+/**
+ * Vercel Node.js Runtime 使用标准的 Node.js HTTP 请求/响应对象 (IncomingMessage, ServerResponse)。
+ * @hono/node-server 的 getRequestListener 可以将 Hono 应用转换为这种标准的 Node.js 请求处理函数。
+ */
+const server = getRequestListener(app.fetch);
+
+module.exports = server;
